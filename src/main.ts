e96b7f90396312bfcc954d20059741f640799783
Gui Seek <guiseek@gmail.com>
2023-03-07 16:16:02 -0300

    feat: máscara de telefone

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import maskForm from './forms/mask.html?raw'
-import './forms/mask'
+import maskFn from './forms/mask'
 import './style.scss'
 
 const app = document.querySelector('#app')
 
 if (app) {
   app.innerHTML = maskForm
+  maskFn()
 }
